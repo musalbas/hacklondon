3ee8a862d11c26ef9d108342722157cb4fe9d36b
philefstat <phil.efstat@gmail.com>
2015-01-17 23:38:16 +0000

    parse fix

--- js/send.js
+++ js/send.js
@@ -1,7 +1,7 @@
 Parse.initialize("9qiSnFq1twgMpo2YcGr1U4kfkyC4mDru6uqK704n", "aT59N4qjCqrE3zh5IfF8bUEpiwEFDRSDnvpQJLWM");  
  $('#myForm').submit(function() {
     // get all the inputs into an array.
-     alert("sent2");
+     alert("sent");
     var $inputs = $('#myForm :input');
 
     // not sure if you wanted this, but I thought I'd add it.
